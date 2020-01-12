ea3fdbede67c2342d092e1ddafc0aae8566e37d5
jorgeabdo <a01194743@itesm.mx>
2020-01-12 03:51:08 -0600

    Firebase Actions Updated

--- src/redux/Actions/authActions.js
+++ src/redux/Actions/authActions.js
@@ -1,3 +1,4 @@
+//Sign in Both Student and Company
 export const signIn = newUser => {
   return (dispatch, getState, getFirebase) => {
     const firebase = getFirebase();
@@ -13,9 +14,61 @@ export const signIn = newUser => {
   };
 };
 
+//SignOut
 export const signOut = () => {
   return (dispatch, getState, getFirebase) => {
     const firebase = getFirebase();
     firebase.auth().signOut();
   };
 };
+
+//Student Sign Up
+export const studentSignUp = newUser => {
+  return (dispatch, getState, getFirebase) => {
+    const firebase = getFirebase();
+    const db = firebase.firestore();
+    db.collection('Usuarios')
+      .where('email', '==', newUser.email)
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          firebase
+            .auth()
+            .createUserWithEmailAndPassword(newUser.email, newUser.password)
+            .then(resp => {
+              return db
+                .collection('Usuarios')
+                .doc(resp.user.uid)
+                .set({
+                  pictureUrl: '',
+                  rol: 'Student',
+                  fName: newUser.fName,
+                  lName: newUser.lName,
+                  mayor: newUser.mayor,
+                  semester: newUser.semester,
+                  desc: newUser.desc
+                });
+            })
+            .then(() => {
+              dispatch({ type: 'SIGNUP_SUCCESS' });
+            })
+            .catch(err => {
+              dispatch({ type: 'SIGNUP_ERROR', err });
+              console.log(err);
+            });
+        }
+        snapshot.forEach(doc => {
+          dispatch({ type: 'USER_ALREADY_EXISTS' });
+        });
+      })
+      .catch(err => {
+        dispatch({ type: 'SIGNUP_ERROR', err });
+      });
+  };
+};
+
+//Company Sign Up
+
+//Update Comapny Profile
+
+//Update Student Profile
---
+++ src/redux/Actions/jobOfferActions.js
@@ -0,0 +1,34 @@
+//Create Job Offer
+export const createJobOffer = jobOffer => {
+  return (dispatch, getState, getFirebase) => {
+    const profile = getState().firebase.profile;
+    const firebase = getFirebase();
+    const db = firebase.firestore();
+    db.collection('JobOffers')
+      .add({
+        companyLogoUrl: profile.companyLogoUrl,
+        name: jobOffer.name,
+        budget: jobOffer.budget,
+        desc: jobOffer.desc,
+        wStart: jobOffer.wStart,
+        wEnd: jobOffer.wEnd,
+        from: jobOffer.from,
+        to: jobOffer.to,
+        createdDate: new Date()
+      })
+      .then(() => {
+        dispatch({ type: 'JOBOFFER_CREATED', jobOffer });
+      })
+      .catch(err => {
+        dispatch({ type: 'JOBOFFER_ERROR', err });
+      });
+  };
+};
+
+//Action for Student Appling for JobOffers
+
+//Action for Company Recomending StudentaJob
+
+//Update Job Offer
+
+//DeleteJobOffer
