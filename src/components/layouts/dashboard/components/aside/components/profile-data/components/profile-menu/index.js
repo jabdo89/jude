34e0ba5285fd08327dd0911fa9ed1795effa9252
ernestognw <ernestognw@gmail.com>
2020-01-22 01:36:00 -0600

    chore(profile-menu): Remove profile button
    Profile button was moved to aside, so it is not necessary here.
    
    No breaking changes

--- src/components/layouts/dashboard/components/aside/components/profile-data/components/profile-menu/index.js
+++ src/components/layouts/dashboard/components/aside/components/profile-data/components/profile-menu/index.js
@@ -3,33 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
-import { Link } from 'react-router-dom';
 import { signOut } from '@actions/authActions';
-import { Container, PseudoContainer, Content, Option, ProfileIcon, ExitIcon } from './elements';
+import { Container, PseudoContainer, Content, Option, ExitIcon } from './elements';
 
 class ProfileMenu extends Component {
-  state = {
-    username: 'abdo'
-  };
-
   handleLogout = () => {
     const { signOut: localeSignout } = this.props;
     localeSignout();
   };
 
   render() {
-    const { username } = this.state;
     const { toggleProfileMenu, show } = this.props;
     return (
       <Container show={show}>
         <PseudoContainer onClick={toggleProfileMenu}>
           <Content>
-            <Link to={`/@${username}`}>
-              <Option>
-                <ProfileIcon />
-                Perfil
-              </Option>
-            </Link>
             <Option onClick={this.handleLogout}>
               <ExitIcon />
               Log out
