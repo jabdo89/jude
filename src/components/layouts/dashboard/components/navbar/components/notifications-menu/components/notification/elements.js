b79a9d22e2ce3163d2d1f12ff54baff272132a03
ernestognw <ernestognw@gmail.com>
2020-01-28 19:11:54 -0600

    feature(notifications): Enabled notifications menu
    Notifications menu was added.
    
    No breaking changes

---
+++ src/components/layouts/dashboard/components/navbar/components/notifications-menu/components/notification/elements.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding: 1rem;
+  color: ${props => props.theme.colors.lightDark};
+
+  * {
+    cursor: pointer;
+  }
+
+  &:hover {
+    transform: translateZ(0px) scale(1.021);
+    transition: transform 0.25s ease-out 0s;
+  }
+`;
+
+const Icon = styled.div`
+  color: ${props => props.theme.colors[props.type]};
+  background-color: ${props => props.theme.colors[props.type]}33;
+  border-radius: 50%;
+  font-size: 16px;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+`;
+
+export { Container, Icon };
